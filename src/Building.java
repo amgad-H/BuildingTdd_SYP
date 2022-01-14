c39e044ce7d12bb14e743e557c289da0e3c4bf94
Amgad <amgadhammash1@gmail.com>
2022-01-14 12:26:45 +0100

    residents can now be added after the creation of a Building

--- src/Building.java
+++ src/Building.java
@@ -9,24 +9,38 @@
  * ----------------------------------------------------------
  */
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Building {
     private int mId;
-    private String[] mResident;
+    private List<String> mResident;
     Building(int id, String resident){
         mId = id;
-        String[] oneResident = {resident};
-        mResident = oneResident;
+        if(mResident == null){
+            mResident = new LinkedList<>();
+        }
+        mResident.add(resident);
     }
     Building(int id, String[] residents){
         mId = id;
-        mResident = residents;
+        if(mResident == null){
+            mResident = new LinkedList<>();
+        }
+        Arrays.stream(residents)
+                .forEach(s -> addResident(s));
     }
 
     public int getId() {
         return mId;
     }
 
-    public String[] getResident() {
+    public List<String> getResident() {
         return mResident;
+    }
+
+    public void addResident(String resident) {
+        mResident.add(resident);
     }
 }
--- tests/BuildingTests.java
+++ tests/BuildingTests.java
@@ -10,6 +10,9 @@
  */
 
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BuildingTests {
@@ -18,10 +21,10 @@ public class BuildingTests {
     void testCreateABuildingWithAnIdAndResident(){
         Building building = new Building(1, "resident");
 
-        String[] gottenResidents = building.getResident();
+        List<String> gottenResidents = building.getResident();
 
         assertEquals(1, building.getId());
-        assertEquals("resident", gottenResidents[0]);
+        assertEquals("resident", gottenResidents.get(0));
     }
 
     @Test
@@ -29,13 +32,31 @@ public class BuildingTests {
         String[] residents = {"r1", "r2"};
         Building building = new Building(1, residents);
 
-        String[] gottenResidents = building.getResident();
+        List<String> gottenResidents = building.getResident();
 
         assertEquals(1, building.getId());
         int i = 0;
         for (String s: residents) {
-            assertEquals(s, gottenResidents[i]);
+            assertEquals(s, gottenResidents.get(i));
             i++;
         }
     }
+
+    @Test
+    void testAddingResidentsAfterTheCreationOfBuilding(){
+        String[] residents = {"r1", "r2"};
+        Building building = new Building(1, residents);
+
+        building.addResident("r3");
+
+        List<String> gottenResidents = building.getResident();
+
+        assertEquals(1, building.getId());
+        int i = 0;
+        for (String s: residents) {
+            assertEquals(s, gottenResidents.get(i));
+            i++;
+        }
+    }
+
 }
